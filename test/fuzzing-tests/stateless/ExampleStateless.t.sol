b8617cdae913a8cab6b784e401e96a6279797ced
Vinicius Cisc <viwdni@gmail.com>
2025-03-22 14:19:26 -0300

    initial commit

--- test/fuzzing-tests/stateless/ExampleStateless.t.sol
+++ test/fuzzing-tests/stateless/ExampleStateless.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.26;
 
-import {BaseTests} from "../../Helpers/BaseTests.t.sol";
+import {BaseTests} from "../../helpers/BaseTests.t.sol";
 
 contract DiamondStateless is BaseTests {
 
